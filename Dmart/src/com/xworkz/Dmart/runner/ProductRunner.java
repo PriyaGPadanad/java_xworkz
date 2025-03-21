50d3adf981c77698e31313bc8ae533ba044882bf
PriyaGPadanad <priyagpadanad@gmail.com>
2025-03-21 11:47:29 +0530

    Dmart info

---
+++ Dmart/src/com/xworkz/Dmart/runner/ProductRunner.java
@@ -0,0 +1,10 @@
+package com.xworkz.Dmart.runner;
+
+import com.xworkz.Dmart.product.ProductInfo;
+
+public class ProductRunner {
+    public static void main(String[] args) {
+        ProductInfo info=new ProductInfo();
+        System.out.printf("Name :"+info.name);
+    }
+}
